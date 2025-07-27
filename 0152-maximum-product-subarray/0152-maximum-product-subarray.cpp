58d07006d80572006118a7479df1c9db5dc51d95
Spectronyx <pt.rj575@gmail.com>
2025-07-27 11:19:11 +0530

    0152-MAXIMUM-PRODUCT-SUBARRAY Stats: Time: 0 ms (100%), Space: 17.7 MB (84.57%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
@@ -13,17 +13,37 @@ public:
         //     }
         // }
 
-        int currMin = arr[0];
-        int currMax = arr[0];
-        int maxProd = arr[0];
+        // int currMin = arr[0];
+        // int currMax = arr[0];
+        // int maxProd = arr[0];
 
-        for(int i = 1 ; i< n;i++){
-            int temp = max({arr[i],arr[i]*currMax,arr[i]*currMin});
+        // for(int i = 1 ; i< n;i++){
+        //     int temp = max({arr[i],arr[i]*currMax,arr[i]*currMin});
             
-            currMin = min({arr[i],arr[i]*currMax,arr[i]*currMin});
+        //     currMin = min({arr[i],arr[i]*currMax,arr[i]*currMin});
 
-            currMax = temp;
-            maxProd = max(maxProd,currMax);
+        //     currMax = temp;
+        //     maxProd = max(maxProd,currMax);
+        // }
+
+        //third approach by traversing in both directions
+        int maxProd = INT_MIN;
+        int lefttoright = 1;
+        int righttoleft = 1;
+
+        for(int i = 0;i < n;i++){
+            if(lefttoright == 0){
+                lefttoright =1;
+            }
+            if(righttoleft == 0){
+                righttoleft = 1;
+            }
+
+            lefttoright *= arr[i];
+
+            righttoleft *= arr[n-i-1];
+
+            maxProd = max({lefttoright,righttoleft,maxProd});
         }
 
         return maxProd;
