dc94a8ca34cf7b6900abb46b85322667fdb22269
Spectronyx <pt.rj575@gmail.com>
2025-07-27 11:09:12 +0530

    0152-MAXIMUM-PRODUCT-SUBARRAY Stats: Time: 0 ms (100%), Space: 17.7 MB (56.59%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
@@ -3,14 +3,27 @@ public:
     int maxProduct(vector<int>& arr) {
     //using the first method
         int n = arr.size();
-        int maxProd = arr[0];
-        for(int i = 0 ; i < n;i++){
-            int mult = 1;
+        // int maxProd = arr[0];
+        // for(int i = 0 ; i < n;i++){
+        //     int mult = 1;
             
-            for(int j = i;j < n;j++){
-                mult *= arr[j];
-                maxProd = max(maxProd,mult);
-            }
+        //     for(int j = i;j < n;j++){
+        //         mult *= arr[j];
+        //         maxProd = max(maxProd,mult);
+        //     }
+        // }
+
+        int currMin = arr[0];
+        int currMax = arr[0];
+        int maxProd = arr[0];
+
+        for(int i = 1 ; i< n;i++){
+            int temp = max({arr[i],arr[i]*currMax,arr[i]*currMin});
+            
+            currMin = min({arr[i],arr[i]*currMax,arr[i]*currMin});
+
+            currMax = temp;
+            maxProd = max(maxProd,currMax);
         }
 
         return maxProd;
