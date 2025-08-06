50dcd9035b184f182a4c659451f847491646d18c
Spectronyx <pt.rj575@gmail.com>
2025-08-06 11:16:02 +0530

    0004-MEDIAN-OF-TWO-SORTED-ARRAYS Stats: Time: 1 ms (40.2%), Space: 95.1 MB (60.41%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -1,40 +1,42 @@
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        int m = nums1.size();
-        int n = nums2.size();
-        
-        if(m > n){
+
+        if(nums1.size() > nums2.size()){
             return findMedianSortedArrays(nums2,nums1);
         }
 
-        int low = 0;
-        int high = m;
-        while(low <= high){
-            int cut1 = (low+high)/2;
-            int cut2 = (m+n+1)/2-cut1;
+        int m = nums1.size();
+        int n = nums2.size();
 
-            //left and right element aroudn the cuts
-            int left1 = (cut1 == 0)? INT_MIN : nums1[cut1-1];
-            int left2 = (cut2 == 0)? INT_MIN : nums2[cut2-1];
+        int l = 0;
+        int r = m;
 
-            int right1 = (cut1 == m)? INT_MAX : nums1[cut1];
-            int right2 = (cut2 == n)? INT_MAX : nums2[cut2];
+        while(l <= r){
+            int Px = l+(r-l)/2; // mid from num1;
+            int Py = (m+n+1)/2 -Px;
+            
+            //left half waale
+            int x1 = (Px == 0 )? INT_MIN: nums1[Px -1];
+            int x2 = (Py == 0 )? INT_MIN: nums2[Py -1];
 
-            if(left1 <= right2 && left2 <= right1){
-                //found right position and now return median
-                if((n+m)%2 == 0){
-                    return (max(left1,left2)+min(right1,right2))/2.00;
-                }else{
-                    return max(left1,left2);
+            //right half waale
+            int x3 = (Px == m)? INT_MAX: nums1[Px];
+            int x4 = (Py == n)? INT_MAX: nums2[Py];
+
+            if(x1 <= x4 && x2 <= x3){
+                if((m+n)%2 == 1){
+                    return max(x1,x2);
                 }
-            }else if(left1 > right2){
-                high = cut1-1;
-            }else{
-                low = cut1+1;
+                return (max(x1,x2)+min(x3,x4))/2.0;
             }
-        }
+            if(x1 > x4){
+                r = Px-1;
+            }else{
+                l = Px+1;
+            }
 
-        return 0.0;
+        }
+        return -1;
     }
 };
