6a7ef1063340465a29dae6396cc3b45695233e99
Spectronyx <pt.rj575@gmail.com>
2025-11-09 15:16:25 +0530

    0812-ROTATE-STRING Stats: Time: 1 ms (21.39%), Space: 42.6 MB (9.7%) - LeetHub

--- 0812-rotate-string/0812-rotate-string.java
+++ 0812-rotate-string/0812-rotate-string.java
@@ -1,17 +1,57 @@
 class Solution {
+    // Step 1: Compute LPS (Longest Prefix Suffix)
+    static int[] computeLPS(String pat) {
+        int n = pat.length();
+        int[] lps = new int[n];
+        int len = 0; // length of previous longest prefix suffix
+        lps[0] = 0;
+
+        int i = 1;
+        while (i < n) {
+            if (pat.charAt(i) == pat.charAt(len)) {
+                len++;
+                lps[i] = len;
+                i++;
+            } else {
+                if (len != 0) {
+                    len = lps[len - 1];
+                } else {
+                    lps[i] = 0;
+                    i++;
+                }
+            }
+        }
+        return lps;
+    }
+
+    // Step 2: Use KMP to check if goal exists in s+s
     public boolean rotateString(String s, String goal) {
-        int n = s.length();
-        int m = goal.length();
+        if (s.length() != goal.length()) return false;
+        String txt = s + s; // concatenated string
+        String pat = goal;
 
-        if( n != m) return false;
+        int n = txt.length();
+        int m = pat.length();
 
-        for(int i = 0 ;i < n;i++){
-            if(s.equals(goal)){
-                return true;
+        int[] lps = computeLPS(pat);
+        int i = 0; // pointer for txt
+        int j = 0; // pointer for pat
+
+        while (i < n) {
+            if (txt.charAt(i) == pat.charAt(j)) {
+                i++;
+                j++;
             }
 
-            char last = s.charAt(n-1);
-            s = last+ s.substring(0,n-1);
+            if (j == m) {
+                return true; // pattern found
+            } else if (i < n && txt.charAt(i) != pat.charAt(j)) {
+                if (j != 0) {
+                    j = lps[j - 1];
+                } else {
+                    i++;
+                }
+            }
         }
         return false;
     }
