e9909386ef76fe9359ee0f3d9ae20ccf3d6be42a
Spectronyx <pt.rj575@gmail.com>
2025-11-13 20:57:52 +0530

    1250-LONGEST-COMMON-SUBSEQUENCE Stats: Time: 23 ms (80.35%), Space: 27.4 MB (68.16%) - LeetHub

--- 1250-longest-common-subsequence/1250-longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/1250-longest-common-subsequence.cpp
@@ -9,8 +9,25 @@ public:
 
        return v[i][j] = max(solve(a,b,i,j+1,v),solve(a,b,i+1,j,v)); 
     }
+
+    int solveTab(string a,string b){
+        int n = a.length();
+        int m = b.length();
+        vector<vector<int>> dp(a.length()+1, vector<int>(b.length()+1,0));
+        for(int i = 1 ;i <= n;i++){
+            for(int j = 1;j<= m;j++){
+                if(a[i-1] == b[j-1]){
+                    dp[i][j] = 1+dp[i-1][j-1];
+                } 
+                else{
+                    dp[i][j] = max(dp[i][j-1],dp[i-1][j]);
+                }
+            }
+        }
+        return dp[n][m];
+    }
     int longestCommonSubsequence(string text1, string text2) {
-        vector<vector<int>> v(text1.length(), vector<int>(text2.length(),-1));
-        return solve(text1,text2,0,0,v);
+        // vector<vector<int>> v(text1.length(), vector<int>(text2.length(),-1));
+        return solveTab(text1,text2);
     }
 };
