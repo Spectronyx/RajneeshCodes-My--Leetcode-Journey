cb15689d7430abc4931284bd72423a949094c8d2
Spectronyx <pt.rj575@gmail.com>
2025-12-11 21:58:30 +0530

    Time: 0 ms (100%), Space: 21.6 MB (73.87%) - LeetHub

--- LeetCode/Medium/0057-insert-interval/0057-insert-interval.cpp
+++ LeetCode/Medium/0057-insert-interval/0057-insert-interval.cpp
@@ -1,21 +1,46 @@
 class Solution {
 public:
+    // vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
+    //     int i = 0;
+
+    //     while(i < intervals.size()){
+                // // i ko age badhana hai
+    //         if(intervals[i][1]< newInterval[0]){
+    //             i++;
+    //         }else if(intervals[i][0] > newInterval[1]){ //interval ko insert karna hai aur
+    //             intervals.insert(intervals.begin() + i,newInterval);
+    //             return intervals;
+    //         }else{ // interval ko merge karna hai
+    //             newInterval[0] = min(newInterval[0],intervals[i][0]);
+    //             newInterval[1] = max(newInterval[1],intervals[i][1]);
+    //             intervals.erase(intervals.begin() + i);
+    //         }
+    //     }
+    //     intervals.push_back(newInterval);
+    //     return intervals;
+    // }
     vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
         int i = 0;
+        int n = intervals.size();
 
-        while(i < intervals.size()){
-            if(intervals[i][1]< newInterval[0]){
-                i++;
+        vector<vector<int>> res;
+        while(i < n){
+            if(intervals[i][1] < newInterval[0]){
+                res.push_back(intervals[i]);
             }else if(intervals[i][0] > newInterval[1]){
-                intervals.insert(intervals.begin() + i,newInterval);
-                return intervals;
+                break;
             }else{
                 newInterval[0] = min(newInterval[0],intervals[i][0]);
                 newInterval[1] = max(newInterval[1],intervals[i][1]);
-                intervals.erase(intervals.begin() + i);
             }
+            i++;
         }
-        intervals.push_back(newInterval);
-        return intervals;
+
+        res.push_back(newInterval);
+        while(i < n){
+            res.push_back(intervals[i]);
+            i++;
+        }
+        return res;
     }
 };
