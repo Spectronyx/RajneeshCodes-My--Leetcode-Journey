7af298b63d90e3ab88da842b6076d356bf6c5cf3
Spectronyx <pt.rj575@gmail.com>
2025-12-11 10:54:29 +0530

    Time: 2 ms (65.54%), Space: 18 MB (31.58%) - LeetHub

--- LeetCode/Easy/2389-longest-subsequence-with-limited-sum/2389-longest-subsequence-with-limited-sum.cpp
+++ LeetCode/Easy/2389-longest-subsequence-with-limited-sum/2389-longest-subsequence-with-limited-sum.cpp
@@ -1,5 +1,23 @@
 class Solution {
 public:
+    int binary_search(vector<int>&nums,int n,int target ){
+        int l = 0,r = n-1;
+        int resIndex = -1;
+        int mid;
+
+        while(l <= r){
+            mid = l+(r-l)/2;
+
+            if(target >= nums[mid]){
+                resIndex = mid;
+                l = mid+1;
+            }
+            else{
+                r= mid-1;
+            }  
+        }
+        return resIndex+1;
+    }
     vector<int> answerQueries(vector<int>& nums, vector<int>& queries) {
         int n = nums.size();
         vector<int> res;
@@ -11,7 +29,7 @@ public:
         }
 
         for(int query: queries){
-            int count = upper_bound(begin(nums),end(nums),query) - nums.begin();
+            int count = binary_search(nums,n,query);
             res.push_back(count);
         }
         return res;
