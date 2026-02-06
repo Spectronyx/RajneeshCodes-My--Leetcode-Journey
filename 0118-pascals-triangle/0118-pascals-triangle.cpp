6b906a04bf34d1b6f340cb2e0a6584acce37eaaa
Spectronyx <pt.rj575@gmail.com>
2026-02-06 12:15:55 +0530

    Time: 0 ms (100%), Space: 9.6 MB (89.27%) - LeetHub

---
+++ 0118-pascals-triangle/0118-pascals-triangle.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<vector<int>> generate(int numRows) {
+        vector<vector<int>> triangle;
+
+        for(int i = 0;i < numRows;i++){
+            vector<int> row(i+1,1);
+            for(int j = 1;j< i;j++){
+                //Each element is sum of above two element of row
+                row[j] = triangle[i-1][j]+triangle[i-1][j-1];
+            }
+            //add the current row the triangle
+            triangle.push_back(row);
+        }
+        return triangle;
+    }
+};
