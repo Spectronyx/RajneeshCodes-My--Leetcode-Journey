8ba15d6a72dfd27526c2b3bcc78d5d943bb7a7b5
Spectronyx <pt.rj575@gmail.com>
2026-03-22 22:37:39 +0530

    Time: 4 ms (35.77%) | Memory: 9.5 MB (99.16%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -2,11 +2,27 @@ class Solution {
 public:
     bool isAnagram(string s, string t) {
         //  brute force approach
-        if(s.length() != t.length()) return false;
+        // if(s.length() != t.length()) return false;
 
-        sort(s.begin(),s.end());
-        sort(t.begin(),t.end());
+        // sort(s.begin(),s.end());
+        // sort(t.begin(),t.end());
 
-        return s == t;
+        // return s == t;
+
+        // optimal approach
+        int arr[26] = {0};
+
+        for(char ch : s){
+            arr[ch-'a']++;
+        }
+
+        for(char ch: t){
+            arr[ch -'a']--;
+        }
+
+        for(int num: arr){
+            if(num != 0) return false;
+        }
+        return true;
     }
 };
