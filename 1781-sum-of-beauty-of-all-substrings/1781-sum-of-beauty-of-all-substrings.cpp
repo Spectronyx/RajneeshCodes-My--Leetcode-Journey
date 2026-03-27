c2d1fad279378169c067b6f48cef2d46c301f403
Spectronyx <pt.rj575@gmail.com>
2026-03-27 14:42:19 +0530

    Time: 55 ms (90.65%), Space: 11.2 MB (48.38%) - LeetHub

--- 1781-sum-of-beauty-of-all-substrings/1781-sum-of-beauty-of-all-substrings.cpp
+++ 1781-sum-of-beauty-of-all-substrings/1781-sum-of-beauty-of-all-substrings.cpp
@@ -1,30 +1,60 @@
+// class Solution {
+// public:
+//     int solve(string &s,int i, int j){
+//         vector<int> freq(26,0);
+//         for(int start = i;start <= j;start++){
+//             freq[s[start]-'a']++;
+//         }
+
+//         int mini = INT_MAX;
+//         int maxi = INT_MIN;
+//         for(auto it: freq){
+//             if(it > 0){
+//                 mini = min(mini,it);
+//                 maxi = max(maxi,it);
+//             }
+//         }
+//         return maxi-mini;
+//     }
+//     int beautySum(string s) {
+//         int n = s.size();
+//         int ans = 0;
+
+//         for(int i =0;i< n;i++){
+//             for(int j = i;j < n;j++){
+//                 ans += solve(s,i,j);
+//             }
+//         }
+//         return ans;
+//     }
+// };
+
 class Solution {
 public:
-    int solve(string &s,int i, int j){
-        vector<int> freq(26,0);
-        for(int start = i;start <= j;start++){
-            freq[s[start]-'a']++;
-        }
-
-        int mini = INT_MAX;
-        int maxi = INT_MIN;
-        for(auto it: freq){
-            if(it > 0){
-                mini = min(mini,it);
-                maxi = max(maxi,it);
-            }
-        }
-        return maxi-mini;
-    }
     int beautySum(string s) {
         int n = s.size();
         int ans = 0;
 
-        for(int i =0;i< n;i++){
-            for(int j = i;j < n;j++){
-                ans += solve(s,i,j);
+        for (int i = 0; i < n; i++) {
+            vector<int> freq(26, 0);
+
+            for (int j = i; j < n; j++) {
+                freq[s[j] - 'a']++;
+
+                int mini = INT_MAX;
+                int maxi = INT_MIN;
+
+                for (int f : freq) {
+                    if (f > 0) {
+                        mini = min(mini, f);
+                        maxi = max(maxi, f);
+                    }
+                }
+
+                ans += (maxi - mini);
             }
         }
+
         return ans;
     }
 };
