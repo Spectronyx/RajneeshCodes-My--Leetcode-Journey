ef40deb8c44a5cb9d796ae8d278447ca2208bce4
Spectronyx <pt.rj575@gmail.com>
2026-04-06 22:39:57 +0530

    Time: 0 ms (100.00%) | Memory: 9.9 MB (63.13%) - LeetSync

--- 908-middle-of-the-linked-list/middle-of-the-linked-list.cpp
+++ 908-middle-of-the-linked-list/middle-of-the-linked-list.cpp
@@ -10,29 +10,44 @@
  */
 class Solution {
 public:
-    ListNode* middleNode(ListNode* head) {
-        int count = 0;
+    // ListNode* middleNode(ListNode* head) {
+    //     int count = 0;
 
+    //     if(head == nullptr || head->next == nullptr) return head;
+
+    //     ListNode *temp = head;
+    //     while(temp != nullptr){
+    //         temp = temp->next;
+    //         count++;
+    //     }
+
+    //     int mid = count/2+1;
+
+    //     temp = head;
+
+    //     while(temp != nullptr){
+    //         mid = mid-1;
+
+    //         if(mid == 0) break;
+
+    //         temp = temp->next;
+    //     }
+
+    //     return temp;
+    // }
+
+        ListNode* middleNode(ListNode* head) {
         if(head == nullptr || head->next == nullptr) return head;
 
-        ListNode *temp = head;
-        while(temp != nullptr){
-            temp = temp->next;
-            count++;
-        }
+        ListNode* slow = head;
+        ListNode* fast = head;
 
-        int mid = count/2+1;
+        while(fast!=nullptr && fast->next != nullptr){
+            slow = slow->next;
 
-        temp = head;
+            fast = fast->next->next;
 
-        while(temp != nullptr){
-            mid = mid-1;
-
-            if(mid == 0) break;
-
-            temp = temp->next;
-        }
-
-        return temp;
+        } 
+        return slow;
     }
 };
