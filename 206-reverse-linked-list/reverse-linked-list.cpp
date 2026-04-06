c066a07f4b5807efdd92483590ba77731247a80c
Spectronyx <pt.rj575@gmail.com>
2026-04-06 23:42:42 +0530

    Time: 0 ms (100.00%) | Memory: 13.5 MB (16.69%) - LeetSync

--- 206-reverse-linked-list/reverse-linked-list.cpp
+++ 206-reverse-linked-list/reverse-linked-list.cpp
@@ -10,6 +10,7 @@
  */
 class Solution {
 public:
+    // using stack
     // ListNode* reverseList(ListNode* head) {
 
     //     ListNode* temp = head;
@@ -31,22 +32,46 @@ public:
         
     //     return head;
     // }
-   ListNode* reverseList(ListNode* head) {
+    // using iterative method;
+    // ListNode* reverseList(ListNode* head) {
 
-    // iterative apporach
-    ListNode *prev = nullptr;
+    //     // iterative apporach
+    //     ListNode *prev = nullptr;
 
-    ListNode *temp = head;
+    //     ListNode *temp = head;
 
-    while(temp != nullptr){
-        ListNode* front = temp->next;
+    //     while(temp != nullptr){
+    //         ListNode* front = temp->next;
 
-        temp->next = prev;
+    //         temp->next = prev;
 
-        prev = temp;
+    //         prev = temp;
 
-        temp = front;
+    //         temp = front;
+    //     }
+    //         return prev;
+    // }
+
+    // using recursion
+    ListNode* reverseList(ListNode* head){
+        // base case if the list has one only one node or no node
+        if(head == nullptr || head->next == nullptr) return head;
+
+        // recursively reverse the list
+        ListNode* newHead = reverseList(head->next);
+
+        // store the next node
+        ListNode *front = head->next;
+
+        // make the next node point to current node;
+
+        front->next = head;
+
+        // break the current node forward linked
+        head->next = nullptr;
+        return newHead;
     }
-        return prev;
-    }
+
+
+
 };
