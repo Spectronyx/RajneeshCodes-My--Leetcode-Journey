54a31e64a3577b16162b84b0b0c64d92f133c118
Spectronyx <pt.rj575@gmail.com>
2026-04-10 18:50:52 +0530

    Time: 45 ms (97.56%) | Memory: 23.9 MB (84.41%) - LeetSync

--- 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.cpp
+++ 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.cpp
@@ -22,19 +22,33 @@ public:
     //     return nullptr;
     // }
 
-    ListNode * getIntersectionNode(ListNode* headA, ListNode* headB){
-        unordered_set<ListNode*> st;
-        while(headA != nullptr){
-            st.insert(headA);
-            headA = headA->next;
-        }
+    // ListNode * getIntersectionNode(ListNode* headA, ListNode* headB){
+    //     unordered_set<ListNode*> st;
+    //     while(headA != nullptr){
+    //         st.insert(headA);
+    //         headA = headA->next;
+    //     }
 
-        while(headB != nullptr){
-            if(st.find(headB) != st.end()){
-                return headB;
-            }
-            headB = headB->next;
+    //     while(headB != nullptr){
+    //         if(st.find(headB) != st.end()){
+    //             return headB;
+    //         }
+    //         headB = headB->next;
+    //     }
+    //     return nullptr;
+    // }
+
+    ListNode * getIntersectionNode(ListNode* headA,ListNode* headB){
+        if(headA == nullptr || headB == nullptr) return nullptr;
+
+        ListNode* temp1 = headA;
+        ListNode* temp2 = headB;
+
+        while(temp1 != temp2){
+            temp1 = temp1 == nullptr ? headB : temp1->next;
+            temp2 = temp2 == nullptr ? headA: temp2->next;
         }
-        return nullptr;
+        return temp1;
     }
+    
 };
