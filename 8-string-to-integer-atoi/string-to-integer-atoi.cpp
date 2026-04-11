4e12f15286a0f31920f5902d50ba3c4830bc91a7
Spectronyx <pt.rj575@gmail.com>
2026-04-12 00:07:16 +0530

    Time: 0 ms (100.00%) | Memory: 9.3 MB (21.73%) - LeetSync

--- 8-string-to-integer-atoi/string-to-integer-atoi.cpp
+++ 8-string-to-integer-atoi/string-to-integer-atoi.cpp
@@ -1,40 +1,37 @@
 class Solution {
 public:
-
     const int INT_MIN_VAL = INT_MIN;
     const int INT_MAX_VAL = INT_MAX;
 
-    // recruseive helper
-    int helper(string &s, int i, long long num, int sign){
-        // if we are you out of bounds or at a non digit, return
-        if(i >= s.size() || !isdigit(s[i])){
-            return (int)(sign*num);
-        }
+    int helper(string &s, int i, long long num,int sign){
+        // if we are out of bound and non char
+        if(i >= s.size() || !isdigit(s[i])) return (int)(num*sign);
 
         // update the num with current digit
-        num = num*10 +(s[i] - '0');
+        num = num* 10 + (s[i]-'0');
 
-        // clamp if overflow
-        if(sign * num <= INT_MIN_VAL) return INT_MIN_VAL;
+        // clamp if overflow 
+        if(sign *num <= INT_MIN_VAL) return INT_MIN_VAL;
         if(sign * num >= INT_MAX_VAL) return INT_MAX_VAL;
 
-        // return the helper for the next character
         return helper(s,i+1,num,sign);
+
     }
 
     int myAtoi(string s) {
+        // ignore the whitespaces
         int i = 0;
+        while(i< s.size() && s[i] == ' '){
+            i++;
+        }
 
-        // skip leading whitespaces
-        while(i < s.size() && s[i] == ' ') i++;
-
-        // handle sign 
+        // handle the sign 
         int sign = 1;
-        if(i < s.size() && (s[i] == '+' || s[i]== '-')){
+        if( i< s.size() && (s[i] == '-' || s[i] == '+')){
             sign = (s[i] == '-') ? -1 : 1;
             i++;
         }
-        // callign the recrusive helperr
+
         return helper(s,i,0,sign);
     }
 };
