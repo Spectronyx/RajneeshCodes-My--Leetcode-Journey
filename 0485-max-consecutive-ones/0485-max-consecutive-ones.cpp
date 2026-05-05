bfa0ecdceb7895bac5b986d578ede966cea3591e
Spectronyx <pt.rj575@gmail.com>
2026-05-06 01:38:39 +0530

    Time: 3 ms (18.94%), Space: 50.1 MB (92.63%) - LeetHub

--- 0485-max-consecutive-ones/0485-max-consecutive-ones.cpp
+++ 0485-max-consecutive-ones/0485-max-consecutive-ones.cpp
@@ -6,14 +6,21 @@ public:
         int n = nums.size();
         int maxLen = 0;
 
-        while(right < n){
-            if(nums[right] != 1){
-                left = right+1;
-                right++;
-            }else{
-                maxLen = max(maxLen,right-left+1);
-                right++;
+        // while(right < n){
+        //     if(nums[right] != 1){
+        //         left = right+1;
+        //         right++;
+        //     }else{
+        //         maxLen = max(maxLen,right-left+1);
+        //         right++;
+        //     }
+        // }
+
+        for(right = 0;right<n;right++){
+            if(nums[right] == 0){
+                left = right + 1;
             }
+            maxLen = max(maxLen,right-left+1);
         }
 
         return  maxLen;
