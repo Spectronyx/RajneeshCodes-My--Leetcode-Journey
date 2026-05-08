d66f0dbba4e14ce88098f2e3758f1f52fe128446
Spectronyx <pt.rj575@gmail.com>
2026-05-08 09:57:20 +0530

    Time: 0 ms (100%), Space: 33.1 MB (24.93%) - LeetHub

--- 2942-find-words-containing-character/2942-find-words-containing-character.cpp
+++ 2942-find-words-containing-character/2942-find-words-containing-character.cpp
@@ -1,17 +1,23 @@
 class Solution {
 public:
-    bool count(string &s,char target){
-        for(auto ch: s){
-            if(ch == target){
-                return true;
-            }
-        }
-        return false;
-    }
+    // bool count(string &s,char target){
+    //     for(auto ch: s){
+    //         if(ch == target){
+    //             return true;
+    //         }
+    //     }
+    //     return false;
+    // }
     vector<int> findWordsContaining(vector<string>& words, char x) {
         vector<int> ans;
+        // for(int i = 0;i < words.size();i++){
+        //     if(count(words[i],x)){
+        //         ans.push_back(i);
+        //     }
+        // }
+
         for(int i = 0;i < words.size();i++){
-            if(count(words[i],x)){
+            if(words[i].find(x) != string::npos){
                 ans.push_back(i);
             }
         }
