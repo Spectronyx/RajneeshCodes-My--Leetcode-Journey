289fecdcc318a764af8e1bf837a429de88ff7a73
Spectronyx <pt.rj575@gmail.com>
2026-05-08 22:18:25 +0530

    Time: 3 ms (31.26%) | Memory: 13.5 MB (24.34%) - LeetSync

--- 137-single-number-ii/single-number-ii.cpp
+++ 137-single-number-ii/single-number-ii.cpp
@@ -1,16 +1,47 @@
 class Solution {
 public:
-    int singleNumber(vector<int>& nums) {
-        unordered_map<int,int> mp;
-        for(auto num: nums){
-            mp[num]++;
-        }
+    // int singleNumber(vector<int>& nums) {
+    //     unordered_map<int,int> mp;
+    //     for(auto num: nums){
+    //         mp[num]++;
+    //     }
 
-        for(auto &p : mp){
-            if(p.second == 1){
-                return p.first;
-            }
+    //     for(auto &p : mp){
+    //         if(p.second == 1){
+    //             return p.first;
+    //         }
+    //     }
+    //     return -1;
+    // }
+
+    //  int singleNumber(vector<int>& nums) {
+    //     int ans = 0;
+        
+    //     // Check each of the 32 bits
+    //     for(int i = 0; i < 32; i++) {
+    //         int count = 0;
+            
+    //         // Count how many numbers have this bit set
+    //         for(int num : nums) {
+    //             if(num & (1 << i)) {
+    //                 count++;
+    //             }
+    //         }
+            
+    //         // If count is not multiple of 3, this bit belongs to the single number
+    //         if(count % 3) {
+    //             ans |= (1 << i);
+    //         }
+    //     }
+        
+    //     return ans;
+    // }
+
+    int singleNumber(vector<int>& nums){
+        sort(nums.begin(),nums.end());
+        for(int i = 1;i < nums.size();i = i+3){
+            if(nums[i-1] != nums[i]) return nums[i-1];
         }
-        return -1;
+        return nums[nums.size()-1];
     }
 };
