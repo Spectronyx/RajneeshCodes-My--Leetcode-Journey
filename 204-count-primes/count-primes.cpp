029b4351e7ba977c147eca89d4f4d0bd05944371
Spectronyx <pt.rj575@gmail.com>
2026-05-10 02:33:54 +0530

    Time: 119 ms (96.98%) | Memory: 132.7 MB (7.35%) - LeetSync

--- 204-count-primes/count-primes.cpp
+++ 204-count-primes/count-primes.cpp
@@ -1,3 +1,4 @@
+// brute force method with optimization
 // class Solution {
 // public:
 //     bool isPrime(int num){
@@ -24,33 +25,64 @@
 //     }
 // };
 
+
+// using the sieve of erastrothenis
+// class Solution {
+// public:
+//     int countPrimes(int n){
+//         // handle the edge case;
+//         if(n <= 2) return 0;
+
+//         // boolean array lo aur sabko prime man lo
+//         vector<bool> isPrime(n,true);
+
+//         isPrime[0] = false;
+//         isPrime[1] = false;
+
+//         // Sieve logic - multiples ko false mark kar do
+//         for(int i = 2;i*i <= n;i++){
+//             if(isPrime[i]){
+//                 // agar i prime h to uske sare multiple ko false mark;
+//                 // start from i*i because usse chhote sab mark hai;
+
+//                 for(int j = i*i; j < n;j += i){
+//                     isPrime[j] = false;
+//                 }
+//             }
+//         }
+//         int count = 0;
+//         for(auto i: isPrime){
+//             if(i == true) count++;
+//         }
+//         return count;
+//     }
+// };
+
+
+// using optimzed sieve
 class Solution {
 public:
-    int countPrimes(int n){
-        // handle the edge case;
+    int countPrimes(int n) {
         if(n <= 2) return 0;
-
-        // boolean array lo aur sabko prime man lo
-        vector<bool> isPrime(n,true);
-
-        isPrime[0] = false;
-        isPrime[1] = false;
-
-        // Sieve logic - multiples ko false mark kar do
-        for(int i = 2;i*i <= n;i++){
-            if(isPrime[i]){
-                // agar i prime h to uske sare multiple ko false mark;
-                // start from i*i because usse chhote sab mark hai;
-
-                for(int j = i*i; j < n;j += i){
-                    isPrime[j] = false;
+        
+        // vector<int> sabse safe hai
+        vector<int> isPrime(n, 1);  // 1 = prime, 0 = not prime
+        
+        isPrime[0] = isPrime[1] = 0;
+        
+        for(int i = 2; i * i < n; i++) {
+            if(isPrime[i]) {
+                for(int j = i * i; j < n; j += i) {
+                    isPrime[j] = 0;
                 }
             }
         }
+        
         int count = 0;
-        for(auto i: isPrime){
-            if(i == true) count++;
+        for(int i = 2; i < n; i++) {
+            count += isPrime[i];  // Direct addition (1 for primes)
         }
+        
         return count;
     }
 };
