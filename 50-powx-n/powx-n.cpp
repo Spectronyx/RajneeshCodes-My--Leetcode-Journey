3278decf70e2a77884a8d9cc27bdf84771d9657f
Spectronyx <pt.rj575@gmail.com>
2026-05-10 14:29:02 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (42.82%) - LeetSync

--- 50-powx-n/powx-n.cpp
+++ 50-powx-n/powx-n.cpp
@@ -1,16 +1,20 @@
 class Solution {
 public:
-    double helper(double x,long n){
-        if(n == 0) return 1;
-
-        if(n < 0) return helper(1/x,-n);
-
-        if(n%2 == 0){
-            return helper(x*x,n/2);
-        }
-        return x*helper(x*x,(n-1)/2);
-    }
     double myPow(double x, int n) {
-        return helper(x,(long)n);
+        if(n == 0) return 1;
+        if(n == 1) return x;
+        if(n == -1) return 1/x;
+
+        double half = myPow(x,n/2);
+
+        if((n&1) == 0){
+            return half*half;
+        }else{
+            if(n > 0){
+                return half*half*x;
+            }else{
+                return half*half*((1/x));
+            }
+        }
     }
 };
