72331fd41d674f5bcbe0d794c9d295c96a209da1
Spectronyx <pt.rj575@gmail.com>
2026-05-11 20:28:56 +0530

    Time: 0 ms (100.00%) | Memory: 10.1 MB (5.48%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.cpp
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -1,25 +1,51 @@
-class Solution {
-public:
-    bool isValid(string s) {
-        stack<char> st;
+// class Solution {
+// public:
+//     bool isValid(string s) {
+//         stack<char> st;
     
+//         for(auto ch: s){
+//             if(ch == '(' || ch == '{' || ch == '['){
+//                 st.push(ch);
+//             }else{
+//                 if(st.empty()) return false;
+
+//                 char top = st.top();
+
+//                 if(top == '(' && ch == ')'||
+//                     top == '{' && ch == '}'||
+//                     top == '[' && ch == ']'){
+//                         st.pop();
+//                     }else{
+//                         return false;
+//                     }
+//             }      
+//         }
+//         return st.empty();
+//     }
+// };
+
+class Solution{
+public:
+    bool isValid(string s){
+        stack<int> st;
+
         for(auto ch: s){
-            if(ch == '(' || ch == '{' || ch == '['){
-                st.push(ch);
+            if(ch == '['){
+                st.push(']');
+            }else if(ch == '{'){
+                st.push('}');
+            }else if(ch == '('){
+                st.push(')');
             }else{
-                if(st.empty()) return false;
+                // ya to stack empty haai ya fir top match match nahiarta
 
-                char top = st.top();
-
-                if(top == '(' && ch == ')'||
-                    top == '{' && ch == '}'||
-                    top == '[' && ch == ']'){
-                        st.pop();
-                    }else{
-                        return false;
-                    }
-            }      
+                if (st.empty() || st.top() != ch ){
+                    return false;
+                }
+                st.pop();
+            }
         }
         return st.empty();
     }
+    
 };
