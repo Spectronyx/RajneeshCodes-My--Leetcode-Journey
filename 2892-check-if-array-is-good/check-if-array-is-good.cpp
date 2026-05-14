307326b9243188f59eb64bd58add239c4aaf4fc6
Spectronyx <pt.rj575@gmail.com>
2026-05-14 11:32:19 +0530

    Time: 0 ms (100.00%) | Memory: 27.1 MB (44.19%) - LeetSync

--- 2892-check-if-array-is-good/check-if-array-is-good.cpp
+++ 2892-check-if-array-is-good/check-if-array-is-good.cpp
@@ -1,35 +1,40 @@
-// class Solution {
-// public:
-//     bool isGood(vector<int>& nums) {
-//         int maxi = *max_element(nums.begin(),nums.end());
-//         int n = maxi;
-        
-//         if(nums.size()-1 ==  maxi){
-//             int sum = 0;
-//             for(auto num: nums){
-//                 sum += num;
-//             }
-//             int required_sum = (n*(n+1))/2;
-//             if(sum == required_sum) return true;
-//         }
-
-//         return false;
-//     }
-// };
-
 class Solution {
 public:
     bool isGood(vector<int>& nums) {
-        int n = *max_element(nums.begin(),nums.end());
-        if(nums.size() != n+1) return false;
+        int maxi = *max_element(nums.begin(),nums.end());
+        int n = maxi;
         
-       sort(nums.begin(),nums.end());
-       if(n<=0) return false;
+        if(nums.size() != n+1) return false;
 
-       for(int i = 0;i < n;i++){
-            if(nums[i] != i+1) return false;
-       }
+        vector<int> freq(n+2,0);
+        for(auto num: nums){
+            if(num < 1 || num > n) return false;
+            freq[num]++;
+        }
 
-       return nums[n] = n;
+        for(int i = 1;i < n;i++){
+            if(freq[i] != 1) return false;
+        }
+
+        return freq[n] == 2;
+
+        return false;
     }
 };
+
+// class Solution {
+// public:
+//     bool isGood(vector<int>& nums) {
+//         int n = *max_element(nums.begin(),nums.end());
+//         if(nums.size() != n+1) return false;
+        
+//        sort(nums.begin(),nums.end());
+//        if(n<=0) return false;
+
+//        for(int i = 0;i < n;i++){
+//             if(nums[i] != i+1) return false;
+//        }
+
+//        return nums[n] = n;
+//     }
+// };
