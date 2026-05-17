55e79543c37bf163e9e700482324fca227785fcf
Spectronyx <pt.rj575@gmail.com>
2026-05-18 00:38:48 +0530

    Time: 4 ms (32.12%) | Memory: 38.6 MB (47.62%) - LeetSync

--- 1428-jump-game-iii/jump-game-iii.cpp
+++ 1428-jump-game-iii/jump-game-iii.cpp
@@ -1,37 +1,58 @@
+// class Solution {
+// public:
+//     bool canReach(vector<int>& arr, int start) {
+//         int n = arr.size();
+//         vector<bool> visited(n, false);
+//         queue<int> q;
+        
+//         q.push(start);
+//         visited[start] = true;
+        
+//         while(!q.empty()) {
+//             int curr = q.front();
+//             q.pop();
+            
+//             // Goal check: value 0 mil gaya?
+//             if(arr[curr] == 0) {
+//                 return true;
+//             }
+            
+//             // Right jump: i + arr[i]
+//             int right = curr + arr[curr];
+//             if(right < n && !visited[right]) {
+//                 visited[right] = true;
+//                 q.push(right);
+//             }
+            
+//             // Left jump: i - arr[i]
+//             int left = curr - arr[curr];
+//             if(left >= 0 && !visited[left]) {
+//                 visited[left] = true;
+//                 q.push(left);
+//             }
+//         }
+        
+//         return false;  // Kabhi 0 tak nahi pahuche
+//     }
+// };
+
 class Solution {
 public:
+    int n;
+    bool dfs(vector<int> &arr,int i){
+        if(i < 0 || i >= n || arr[i] < 0) return false;
+
+        if(arr[i] == 0) return true;
+
+        arr[i] *= -1;
+        bool a = dfs(arr,i-arr[i]);
+        bool b = dfs(arr,i+arr[i]);
+
+        return a||b;
+    }
     bool canReach(vector<int>& arr, int start) {
-        int n = arr.size();
-        vector<bool> visited(n, false);
-        queue<int> q;
+         n = arr.size();
         
-        q.push(start);
-        visited[start] = true;
-        
-        while(!q.empty()) {
-            int curr = q.front();
-            q.pop();
-            
-            // Goal check: value 0 mil gaya?
-            if(arr[curr] == 0) {
-                return true;
-            }
-            
-            // Right jump: i + arr[i]
-            int right = curr + arr[curr];
-            if(right < n && !visited[right]) {
-                visited[right] = true;
-                q.push(right);
-            }
-            
-            // Left jump: i - arr[i]
-            int left = curr - arr[curr];
-            if(left >= 0 && !visited[left]) {
-                visited[left] = true;
-                q.push(left);
-            }
-        }
-        
-        return false;  // Kabhi 0 tak nahi pahuche
+        return dfs(arr,start);
     }
 };
