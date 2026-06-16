c14304f4d9a571811318cf4d4dbc604b2b1fb73e
Rajneesh Sharma <pt.rj575@gmail.com>
2026-06-16 17:13:23 +0530

    Time: 0 ms (100%), Space: 7.7 MB (98.65%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -2,16 +2,14 @@ class Solution {
 public:
     int arr[46];
     int solve(int n){
-        if(n == 1){
-            return 1;
-        }
+        if(n == 1) return 1;
         if(n == 2) return 2;
 
         if(arr[n] != -1){
             return arr[n];
         }
 
-        return arr[n] =  solve(n-1)+solve(n-2);
+        return arr[n] = solve(n-2)+solve(n-1);
     }
     int climbStairs(int n) {
         memset(arr,-1,sizeof(arr));
